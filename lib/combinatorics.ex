f9f17d836818629821e642b170f3b730e917ba76
Paulo Köch <pkoch@lifeonmars.pt>
2017-08-24 14:51:57 +0100

    Introduce multiply

--- lib/combinatorics.ex
+++ lib/combinatorics.ex
@@ -2,4 +2,8 @@ defmodule Markus.Combinatorics do
   def combine(_, 0), do: [[]]
   def combine([], _), do: []
   def combine([h|t], k), do: (for l <- combine(t, k - 1), do: [h|l]) ++ combine(t, k)
+
+  def multiply([]), do: []
+  def multiply([as]), do: for a <- as, do: [a]
+  def multiply([as|others]), do: for a <- as, rest <- multiply(others), do: [a|rest]
 end
--- test/combinatorics_test.exs
+++ test/combinatorics_test.exs
@@ -27,4 +27,28 @@ defmodule Markus.Combinatorics.Test do
       [1, 2, 2],
     ]
   end
+
+  test "multiply simple example" do
+    assert multiply([[1, 2], ["A", "B"], ["!", "@"]]) == [
+      [1, "A", "!"],
+      [1, "A", "@"],
+      [1, "B", "!"],
+      [1, "B", "@"],
+      [2, "A", "!"],
+      [2, "A", "@"],
+      [2, "B", "!"],
+      [2, "B", "@"],
+    ]
+  end
+
+  test "multiply one enumerable" do
+    assert multiply([[1, 2]]) == [
+      [1],
+      [2],
+    ]
+  end
+
+  test "multiply nothing" do
+    assert multiply([]) == []
+  end
 end
